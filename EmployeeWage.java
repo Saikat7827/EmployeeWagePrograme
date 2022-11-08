5836282c8306e0e861e04d1baa00840083fb4b73
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-11-08 19:15:27 +0530

    Total Working Hrs by the Employee

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,44 +1,38 @@
 package com.bridgelabz.empwage;
 
 public class EmployeeWage {
+	public static final int fullTime = 2;
+	public static final int partTime = 1;
+	public static final int wagePrHr = 20;
+	public static final int workingDays = 20;
+	public static final int maxWorkingHrs = 100;
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Problem");
-		
-		final int fullTime = 2;
-		final int partTime = 1;
-		final int wagePerHr = 20;
-		final int workingDays = 20;
-		int dailyWage = 0;
-		int totalWage = 0;
-		
-		for (int day = 1; day <= workingDays; day++)
-		{
-			System.out.println("FOR DAY " + day);
-			
-			int empCheck = (int) (Math.random() * 100) % 3;
-			
-			int FullDayHours =0;
-			
-			switch (empCheck)
-	        {
-	        case fullTime:
-	        	FullDayHours = 16;
-	        	dailyWage = FullDayHours * wagePerHr;
-	        	System.out.println("Employee is present for full time \n And Salary is " + dailyWage);
-                break;
-            case partTime:
-            	FullDayHours = 8;
-            	dailyWage = FullDayHours * wagePerHr;
-            	System.out.println("Employee is present for part time \n And Salary is " + dailyWage);
-                break;
-            default:
-            	System.out.println("Employee is absent \n And Salary is " + dailyWage);
-            	break;
-            }
-			
-			totalWage += dailyWage;
-	    }
-		System.out.println("Total wage for a month is " + totalWage);
-    }
+		int empHrs = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+
+		while (totalEmpHrs <= maxWorkingHrs && totalWorkingDays < workingDays) {
+			totalWorkingDays++;
+
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+
+			switch (empCheck) {
+			case partTime:
+				empHrs = 4;
+				break;
+			case fullTime:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+			}
+
+			totalEmpHrs += empHrs;
+			System.out.println("For Day" + totalWorkingDays + " : Employee Maximum Working Hours are " + empHrs);
+		}
+		int totalEmpWage = totalEmpHrs * wagePrHr;
+		System.out.println("Total Employee Wage for month is " + totalEmpWage);
+	}
 
 }
