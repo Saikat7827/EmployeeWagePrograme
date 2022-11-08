c273a7455503ebe938b02f7dda157a5517fc9f30
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-11-08 15:25:49 +0530

    Added:Wages of the Employee for full day

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,15 +1,20 @@
 package com.bridgelabz.empwage;
 
 public class EmployeeWage {
-
 	public static void main(String[] args) {
-		
-		System.out.println("Welcome to employee wage computation");
-		int empPresent = (int) (Math.floor(Math.random() * 10) % 2);
-		if (empPresent == 1) {
+
+		final int fullTime = 1;
+		final int wagePerHr = 20;
+		int fullDayHours = 0;
+		int empCheck = (int) (Math.random() * 100) % 2;
+		if (empCheck == fullTime) {
 			System.out.println("Employee is Present");
+			fullDayHours = 8;
 		} else {
 			System.out.println("Employee is Absent");
 		}
+		int wage = fullDayHours * wagePerHr;
+		System.out.println("Employee Daily Wage is " + wage);
 	}
+
 }
